6d1973662d1438f2cefddbad5cc9bf7556371c10
RafaAlbano <rafaelaalbano70@gmail.com>
2023-08-14 09:23:49 -0300

    criação das models

--- ourfood/admin.py
+++ ourfood/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Lanches, Bebidas, Sobremesas
+
+admin.site.register(Lanches)
+admin.site.register(Bebidas)
+admin.site.register(Sobremesas)
