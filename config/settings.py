f8a8c112585d92ce7beb34979cba9634cce43902
RafaAlbano <rafaelaalbano70@gmail.com>
2023-09-25 14:48:24 -0300

    salvando os dados do banco

--- config/settings.py
+++ config/settings.py
@@ -52,8 +52,8 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Livraria API",
-    "DESCRIPTION": "API para gerenciamento de livraria, incluindo endpoints e documentação.",
+    "TITLE": "Ourfood API",
+    "DESCRIPTION": "API para gerenciamento de delivery, incluindo endpoints e documentação.",
     "VERSION": "1.0.0",
 }
 
@@ -151,7 +151,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # App Uploader settings
-MEDIA_URL = "http://191.52.55.182:19003/media/"
+MEDIA_URL = "http://191.52.55.130:19003/media/"
 MEDIA_ENDPOINT = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 FILE_UPLOAD_PERMISSIONS = 0o640
