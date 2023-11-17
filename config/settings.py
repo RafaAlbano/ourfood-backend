e447ed0362982a7b34a6cc373e6a5eeced794996
RafaAlbano <rafaelaalbano70@gmail.com>
2023-11-17 08:39:27 -0300

    CORS arrumado

--- config/settings.py
+++ config/settings.py
@@ -142,8 +142,6 @@ else:
     MY_IP = os.getenv("MY_IP", "127.0.0.1")
     MEDIA_URL = f"http://{MY_IP}:19003/media/"
 
-CORS_ALLOW_ALL_ORIGINS = [
-    "https://*.fl0.io",
-]   
+CORS_ALLOW_ALL_ORIGINS = [True]   
 
 print(MODE, MEDIA_URL, DATABASES)
